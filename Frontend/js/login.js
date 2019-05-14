b05dd53fc518594e9492197d13f582e8af2fe2b0
Xwilarg <xwilarg@yahoo.fr>
2019-05-14 11:54:02 +0200

    Add front login request

---
+++ Frontend/js/login.js
@@ -0,0 +1,25 @@
+function login() {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            if (this.status == 200) {
+                console.log(this.responseText);
+            } else {
+                var now     = new Date(); 
+                var hour    = now.getHours();
+                var minute  = now.getMinutes();
+                var second  = now.getSeconds();   
+                if(hour.toString().length == 1)
+                    hour = '0' + hour;
+                if(minute.toString().length == 1)
+                    minute = '0' + minute;
+                if(second.toString().length == 1)
+                    second = '0' + second;
+                document.getElementById("reply").innerHTML = hour + ":" + minute + ":" + second + ": Username and password doesn't match.";
+            }
+        }
+    });
+    xhr.open("POST", "http://93.118.34.39:5151/auth");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("username=" + document.getElementById("username").value + "&password=" + document.getElementById("password").value);
+}
