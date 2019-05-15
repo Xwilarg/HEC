3da99145a5d693db98ca07490ae41ad90e82f6e9
Xwilarg <xwilarg@yahoo.fr>
2019-05-15 11:07:39 +0200

    Add sample graphs

--- Frontend/js/dashboard.js
+++ Frontend/js/dashboard.js
@@ -11,3 +11,55 @@ xhr.addEventListener("readystatechange", function () {
 xhr.open("POST", "http://93.118.34.39:5151/devices");
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 xhr.send("token=" + sessionStorage['token']);
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawGraphs);
+
+function drawGraphs() {
+    let options = {
+        title: ''
+    };
+
+    let data = google.visualization.arrayToDataTable([
+        ['Data1', 'Data2'],
+        ['Element1', 45],
+        ['Element2', 75],
+        ['Element3', 42],
+        ['Element4', 34],
+        ['Element5', 8]
+    ]);
+
+    options.title = 'Sample Pie Chart';
+    let chart = new google.visualization.PieChart(document.getElementById('consumptionBreakdownGraph'));
+    chart.draw(data, options);
+
+    data = google.visualization.arrayToDataTable([
+        ["Data1", 'Data2'],
+        ["Element1", 43],
+        ["Element2", 25],
+        ["Element3", 52],
+        ["Element4", 12]
+    ]);
+
+    options.title = 'Sample Column Chart';
+    chart = new google.visualization.ColumnChart(document.getElementById('comsumptionPerBuildingsGraph'));
+    chart.draw(data, options);
+
+    data = google.visualization.arrayToDataTable([
+        ["Data1", 'Data2'],
+        ["Element1", 8],
+        ["Element2", 5],
+        ["Element3", 12],
+        ["Element4", 6],
+        ["Element5", 8],
+        ["Element6", 3],
+        ["Element7", 15],
+        ["Element8", 9],
+        ["Element9", 10],
+        ["Element10", 7]
+    ]);
+
+    options.title = 'Sample Line Chart';
+    chart = new google.visualization.LineChart(document.getElementById('comsumptionPerHoursGraph'));
+    chart.draw(data, options);
+}
